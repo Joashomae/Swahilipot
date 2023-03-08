643cfb59f60b86af26d0100011e45090dd9b0a05
iRobot90 <amiandajesse27@gmail.com>
2023-03-08 12:26:36 +0300

    removed firebaseAdmin.js and firebaseConfig.js files

--- index.js/server.js
+++ index.js/server.js
@@ -1,48 +1,52 @@
-// Import required packages
-const express = require("express");
-const firebase = require("firebase");
+const express = require('express');
+const bodyParser = require('body-parser');
+const firebase = require('firebase');
 
-// Initialize express app
+// Initialize Firebase app
+const firebaseConfig = {
+  // Add your Firebase configuration here
+};
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Express app
 const app = express();
-const port = 3000;
 
-// Serve static files from public folder
-app.use(express.static("public"));
+// Parse incoming request bodies as JSON
+app.use(bodyParser.json());
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
 
 // Handle form submission
-app.post("/bookings", async (req, res) => {
-    try {
-        // Extract data from form submission
-        const name = req.body.name;
-        const email = req.body.email;
-        const checkin = req.body.checkin;
-        const checkout = req.body.checkout;
-        const requirements = req.body.requirements;
-        const price = req.body.price;
-        const comments = req.body.comments;
+app.post('/submit', (req, res) => {
+  // Get the form data from the request body
+  const { name, email, checkin, checkinTime, checkout, checkoutTime, spaceType, price, comments } = req.body;
 
-        // Save data to Firebase database
-        const db = firebase.firestore();
-        await db.collection("bookings").add({
-            name: name,
-            email: email,
-            checkin: checkin,
-            checkout: checkout,
-            requirements: requirements,
-            price: price,
-            comments: comments
-        });
-
-        // Send success response
-        res.status(200).send("Booking successful!");
-    } catch (error) {
-        // Send error response
-        console.error(error);
-        res.status(500).send("Internal server error");
-    }
+  // Create a new entry in the Firebase database
+  const bookingRef = firebase.database().ref('bookings').push();
+  bookingRef.set({
+    name,
+    email,
+    checkin,
+    checkinTime,
+    checkout,
+    checkoutTime,
+    spaceType,
+    price,
+    comments,
+  })
+    .then(() => {
+      // Send a success response to the client
+      res.status(200).send('Booking saved successfully!');
+    })
+    .catch((error) => {
+      console.error('Error saving booking:', error);
+      // Send an error response to the client
+      res.status(500).send('Error saving booking.');
+    });
 });
 
-// Start server
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
