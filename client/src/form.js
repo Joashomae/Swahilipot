816535b55cf7f5904c9eb40bc4b5044deee7e681
iRobot90 <amiandajesse27@gmail.com>
2023-03-20 22:09:40 +0300

    added cool css

--- client/src/form.js
+++ client/src/form.js
@@ -1,9 +1,18 @@
-var typed = new Typed(".multiple-text",{
-    strings:["Co-working Space?","Conference-room/Boardroom?","Hall?","Ampitheater?","Training Room?"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay:1000,
-    loop:true,
-    
+const words = ["Hall?", "Work-Space?", "Board/Conference Room?", "Amphitheatre?"];
+let index = 0;
 
-})
+function changeWord() {
+  const text = document.querySelector(".multiple-text");
+  text.textContent = words[index];
+  index++;
+
+  if (index === words.length) {
+    index = 0;
+  }
+
+  setTimeout(() => {
+    changeWord();
+  }, 4000);
+}
+
+changeWord();
